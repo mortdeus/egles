880c43c0ac8883f677e459319d5abd318178ca66
mortdeus <mortdeus@gocos2d.org>
2012-11-05 22:27:26 -0600

    added go doc

---
+++ doc.go
@@ -0,0 +1,19 @@
+/*
+A portable gl layer that aims to abstract and scale. 
+Designed for the Go programming language to use with gocos2d. 
+*/
+package egles
+
+/*
+TODO:
+EGL wrappers 		~30% 		done
+GLES2 wrappers 		 ~5% 		done
+GLES wrappers 		  0% 		done
+
+
+Angle,waffle, wayland, nd andriod support have 
+yet to be tested but are ideas currently on the road map.  
+
+
+
+*/
