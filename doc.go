fd2f13497b63c0c93d551b27820f8740dace7532
mortdeus <mortdeus@gocos2d.org>
2012-11-29 07:09:05 -0600

    removed misc

--- doc.go
+++ doc.go
@@ -1,15 +1,12 @@
 /*
-A portable gl layer that aims to abstract and scale. 
-Designed for the Go programming language to use with gocos2d. 
+A cross platform egl wrapper for golang.  
 */
 package egles
 /*
 TODO:
-EGL wrappers 		~30% 		done
-GLES2 wrappers 		 ~5% 		done
+EGL wrappers 		~95% 		done
+GLES2 wrappers 		 ~95% 		done
 GLES wrappers 		  0% 		done
-
-Angle,waffle, wayland, nd andriod support have 
-yet to be tested but are ideas currently on the road map.  
+  
 */
 type Documentation int
