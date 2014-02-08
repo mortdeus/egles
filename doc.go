08e078978dc0c43a1cc3e6bd54ef432eef4f8b22
mortdeus <mortdeus@gocos2d.org>
2014-02-08 11:09:49 -0600

    cleared doc.go text

--- doc.go
+++ doc.go
@@ -1,15 +1,3 @@
-/*
-A cross platform egl wrapper for golang.
-*/
+//http://mortdeus.mit-license.org
+
 package egles
-
-/*
-TODO:
-EGL wrappers 		~95% 		done
-GLES2 wrappers 		 ~95% 		done
-GLES wrappers 		  0% 		done
-
-*/
-type Documentation int
-
-var Authors = [...]string{"Mortdeus (http://mortdeus.mit-license.org)", "remogatto"}
